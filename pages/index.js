e34c092a0d33d6d4c870f72dc256caf54c72fb08
Abiona Michael <amabiona21@gmail.com>
2021-05-03 12:21:15 +0100

    added the menu section

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Hero from '../components/hero/index'
 import About from '../components/about/index'
+import Menu from '../components/menu'
 
 const Home = () => {
   return (
     <>
       <Hero />
       <About />
+      <Menu />
     </>
   )
 }
