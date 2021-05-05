c973e6812c0e92b430522ed041ebe063be710e9f
Abiona Michael <amabiona21@gmail.com>
2021-05-05 13:35:23 +0100

    changes made in menu

--- components/menu/index.js
+++ components/menu/index.js
@@ -8,27 +8,27 @@ const Menu = () => {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           <div className="relative md:row-span-2">
-            <div className="absolute uppercase py-4 px-6 text-lg rounded-md bg-gray-100 hover:bg-yellow-600 hover:text-gray-100 opacity-90 left-36 top-60 font-semibold">Lunch</div>
+            <div className="absolute uppercase py-4 px-6 text-lg rounded-md bg-gray-100 tracking-widest hover:bg-yellow-600 hover:text-gray-100 opacity-90 left-36 top-60 font-semibold">Lunch</div>
             <img src="/our-menu-01.jpg" className="w-full h-full rounded-lg" alt=""/>
           </div>
           <div className="relative md:row-span-2">
-            <div className="absolute uppercase py-4 px-6 text-lg rounded-md bg-gray-100 hover:bg-yellow-600 hover:text-gray-100 opacity-90 left-36 top-60 font-semibold">Dinner</div>
+            <div className="absolute uppercase py-4 px-6 text-lg rounded-md bg-gray-100 tracking-widest hover:bg-yellow-600 hover:text-gray-100 opacity-90 left-36 top-60 font-semibold">Dinner</div>
             <img src="/our-menu-05.jpg" className="w-full h-full rounded-lg" alt=""/>
           </div>
-          <div className="relative ">
-            <div className="absolute uppercase py-4 px-6 text-lg rounded-md bg-gray-100 hover:bg-yellow-600 hover:text-gray-100 opacity-90 left-36 top-24  font-semibold">Drinks</div>
+          <div className="relative">
+            <div className="absolute uppercase py-4 px-6 text-lg rounded-md bg-gray-100 tracking-widest hover:bg-yellow-600 hover:text-gray-100 opacity-90 left-36 top-24  font-semibold">Drinks</div>
             <img src="/our-menu-08.jpg" className="w-full h-full rounded-lg" alt=""/>
           </div>
-          <div className="relative ">
-            <div className="absolute uppercase py-4 px-6 text-lg rounded-md bg-gray-100 hover:bg-yellow-600 hover:text-gray-100 opacity-90 left-36 top-24  font-semibold">Starters</div>
+          <div className="relative">
+            <div className="absolute uppercase py-4 px-6 text-lg rounded-md bg-gray-100 tracking-widest hover:bg-yellow-600 hover:text-gray-100 opacity-90 left-36 top-24  font-semibold">Starters</div>
             <img src="/our-menu-10.jpg" className="w-full h-full rounded-lg" alt=""/>
           </div>
           <div className="relative md:col-span-2">
-            <div className="absolute uppercase py-4 px-6 text-lg rounded-md bg-gray-100 hover:bg-yellow-600 hover:text-gray-100 opacity-90 left-80 top-24  font-semibold">Happy Hour</div>
+            <div className="absolute uppercase py-4 px-6 text-lg rounded-md bg-gray-100 tracking-widest hover:bg-yellow-600 hover:text-gray-100 opacity-90 left-80 top-24  font-semibold">Happy Hour</div>
             <img src="/our-menu-13.jpg" className="w-full h-full rounded-lg" alt=""/>
           </div>
-          <div className="relative ">
-            <div className="absolute uppercase py-4 px-6 text-lg rounded-md bg-gray-100 hover:bg-yellow-600 hover:text-gray-100 opacity-90 left-36 top-24  font-semibold">Dessert</div>
+          <div className="relative">
+            <div className="absolute uppercase py-4 px-6 text-lg rounded-md bg-gray-100 tracking-widest hover:bg-yellow-600 hover:text-gray-100 opacity-90 left-36 top-24  font-semibold">Dessert</div>
             <img src="/our-menu-16.jpg" className="w-full h-full rounded-lg" alt=""/>
           </div>
         </div>
