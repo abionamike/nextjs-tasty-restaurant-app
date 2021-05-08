aa246dd5a819dca94dd605b981c414cb1d55c007
Abiona Michael <amabiona21@gmail.com>
2021-05-08 08:58:12 +0100

    bug fix

--- components/contact/index.js
+++ components/contact/index.js
@@ -13,11 +13,11 @@ const Contact = () => {
         </div>
         <div className="flex flex-col m-auto items-center justify-center md:space-x-10 lg:space-x-16 md:flex-row w-4/5">
           <img src="/dinner-03.jpg" alt="Contact our customer support" className="rounded-xl md:h-96 md:w-1/2" />
-          <form onSubmit={handleSubmit} className="space-y-6 w-full mt-8 md:mt-0">
+          <form onSubmit={handleSubmit} className="w-full mt-8 md:mt-0">
             <h1 className="title-font ext-gray-bgColor2 text-xl font-semibold">Drop Us A Message!</h1>
-            <input required id="name" type="text" placeholder="Full name" className="w-full p-3 rounded bg-gray-200" />
-            <input required id="email" type="email" placeholder="Email" className="w-full p-3 rounded bg-gray-200" />
-            <textarea required id="message" rows="3" placeholder="Message" className="w-full p-3 rounded bg-gray-200"></textarea>
+            <input required id="name" type="text" placeholder="Full name" className="my-3 w-full p-3 rounded bg-gray-200" />
+            <input required id="email" type="email" placeholder="Email" className="my-3 w-full p-3 rounded bg-gray-200" />
+            <textarea required id="message" rows="3" placeholder="Message" className="my-3 w-full p-3 rounded bg-gray-200"></textarea>
             <button className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded bg-yellow-600 text-yellow-50">Send Message</button>
           </form>
         </div>
--- components/services/index.js
+++ components/services/index.js
@@ -1,12 +1,12 @@
 const Services = () => {
   return ( 
-    <section id="Services" className="text-gray-600 body-font">
-      <div className="container w-10/12 m-auto flex flex-col lg:px-5 py-24 mx-auto">
+    <section id="Services" className="text-gray-600 py-24">
+      <div className="container w-10/12 m-auto">
         <div className="flex items-center justify-center mb-20">
           <div className="h-12 w-1 m-2 bg-yellow-600"></div>
           <h1 className="title-font m-2 text-gray-bgColor2 text-3xl font-semibold">Services</h1>
         </div>
-        <div className="flex flex-col m-auto items-center justify-center md:flex-row text-center">
+        <div className="flex flex-col w-11/12 md:w-full m-auto items-center justify-center md:flex-row text-center">
           <div className="relative w-full mb-10 mx-4 shadow-lg">
             <div className="rounded-lg h-64 overflow-hidden">
               <img alt="content" className="object-cover object-center h-full w-full" src="/booking-02.jpg" />
