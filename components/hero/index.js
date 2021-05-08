0cd7186fadf89fca238208e0975ccd487f533d18
Abiona Michael <amabiona21@gmail.com>
2021-05-08 13:30:13 +0100

    bug fix

--- components/hero/index.js
+++ components/hero/index.js
@@ -31,12 +31,12 @@ const Hero = () => {
                 </button>
               </div>
               <div className={`hidden md:block`}>
-                <a href="#Home" className="font-medium mx-3 lg:mx-4 text-base text-white-color1 hover:text-yellow-500" onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}>Home</a>
-                <a href="#About" className="font-medium mx-3 lg:mx-4 text-base text-white-color1 hover:text-yellow-500" onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}>About Us</a>
-                <a href="#Services" className="font-medium mx-3 lg:mx-4 text-base text-white-color1 hover:text-yellow-500" onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}>Services</a>
-                <a href="#Menu" className="font-medium mx-3 lg:mx-4 text-base text-white-color1 hover:text-yellow-500" onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}>Menu</a>
-                <a href="#Reviews" className="font-medium mx-3 lg:mx-4 text-base text-white-color1 hover:text-yellow-500" onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}>Reviews</a>
-                <a href="#Contact" className="font-medium mx-3 lg:mx-4 text-base text-yellow-500 hover:text-yellow-600" onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}>Contact Us</a>
+                <a href="#Home" className="font-medium mx-4 text-base text-white-color1 hover:text-yellow-500" onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}>Home</a>
+                <a href="#About" className="font-medium mx-4 text-base text-white-color1 hover:text-yellow-500" onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}>About Us</a>
+                <a href="#Services" className="font-medium mx-4 text-base text-white-color1 hover:text-yellow-500" onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}>Services</a>
+                <a href="#Menu" className="font-medium mx-4 text-base text-white-color1 hover:text-yellow-500" onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}>Menu</a>
+                <a href="#Reviews" className="font-medium mx-4 text-base text-white-color1 hover:text-yellow-500" onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}>Reviews</a>
+                <a href="#Contact" className="font-medium mx-4 text-base text-yellow-500 hover:text-yellow-600" onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}>Contact Us</a>
               </div>
               {/* <div className="hidden md:block">
                 <a className="flex items-center justify-center px-2 py-2 border-2 text-yellow-500 border-yellow-500 text-sm font-medium rounded-3xl bg-transparent hover:bg-yellow-500 hover:text-gray-bgColor2" href="#Contact">Contact Us</a>
