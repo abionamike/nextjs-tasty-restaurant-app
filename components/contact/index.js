9a5551e62493a8d3b865ac8f5bcc525285a4528c
Abiona Michael <amabiona21@gmail.com>
2021-05-08 08:07:41 +0100

    bug fix

--- components/contact/index.js
+++ components/contact/index.js
@@ -5,26 +5,26 @@ const Contact = () => {
     alert('Message received :)')
   }
   return (
-    <section id="Contact" class="relative py-16 my-16 md:my-24 md:py-24">
+    <section id="Contact" className="relative py-16 my-16 md:my-24 md:py-24">
       <div className="container flex flex-col items-center justify-center m-auto">
         <div className="flex items-center justify-center md:justify-start mb-20">
           <div className="h-12 w-1 m-2 bg-yellow-600"></div>
           <h1 className="title-font m-2 text-gray-bgColor2 text-3xl font-semibold">Contact Us</h1>
         </div>
         <div className="flex flex-col m-auto items-center justify-center md:space-x-10 lg:space-x-16 md:flex-row w-4/5">
-          <img src="/dinner-03.jpg" alt="Contact our customer support" class="rounded-xl md:h-96 md:w-1/2" />
-          <form onSubmit={handleSubmit} class="space-y-6 w-full mt-8 md:mt-0">
+          <img src="/dinner-03.jpg" alt="Contact our customer support" className="rounded-xl md:h-96 md:w-1/2" />
+          <form onSubmit={handleSubmit} className="space-y-6 w-full mt-8 md:mt-0">
             <h1 className="title-font ext-gray-bgColor2 text-xl font-semibold">Drop Us A Message!</h1>
             <div>
-              <input required id="name" type="text" placeholder="Full name" class="w-full p-3 rounded bg-gray-200" />
+              <input required id="name" type="text" placeholder="Full name" className="w-full p-3 rounded bg-gray-200" />
             </div>
             <div>
-              <input required id="email" type="email" placeholder="Email" class="w-full p-3 rounded bg-gray-200" />
+              <input required id="email" type="email" placeholder="Email" className="w-full p-3 rounded bg-gray-200" />
             </div>
             <div>
-              <textarea required id="message" rows="3" placeholder="Message" class="w-full p-3 rounded bg-gray-200"></textarea>
+              <textarea required id="message" rows="3" placeholder="Message" className="w-full p-3 rounded bg-gray-200"></textarea>
             </div>
-            <button class="w-full p-3 text-sm font-bold tracking-wide uppercase rounded bg-yellow-600 text-yellow-50">Send Message</button>
+            <button className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded bg-yellow-600 text-yellow-50">Send Message</button>
           </form>
         </div>
       </div>
--- components/services/index.js
+++ components/services/index.js
@@ -1,27 +1,27 @@
 const Services = () => {
   return ( 
     <section id="Services" className="text-gray-600 body-font">
-      <div className="container w-10/12 flex flex-col lg:px-5 py-24 mx-auto">
+      <div className="container w-11/12 m-auto flex flex-col lg:px-5 py-24 mx-auto">
         <div className="flex items-center justify-center mb-20">
           <div className="h-12 w-1 m-2 bg-yellow-600"></div>
           <h1 className="title-font m-2 text-gray-bgColor2 text-3xl font-semibold">Services</h1>
         </div>
-        <div className="flex flex-col md:space-x-8 lg:space-x-14 items-center justify-center md:flex-row text-center">
-          <div className="relative md:w-1/4 w-10/12 mb-10 shadow-lg">
+        <div className="flex flex-col m-auto items-center justify-center w-10/12 md:flex-row text-center">
+          <div className="relative md:w-1/4 w-full mb-10 mx-4 shadow-lg">
             <div className="rounded-lg h-64 overflow-hidden">
               <img alt="content" className="object-cover object-center h-full w-full" src="/booking-02.jpg" />
             </div>
             <div className="absolute left-1 top-56 text-center text-xl text-yellow-700 font-semibold rounded-full p-3 bg-gray-100 shadow-lg">01.</div>
             <h2 className="title-font text-lg font-medium text-gray-900 mt-6 mb-3">Dine/Take Away</h2>
           </div>
-          <div className="relative md:w-1/4 w-10/12 mb-10 shadow-lg">
+          <div className="relative md:w-1/4 w-full mb-10 mx-4 shadow-lg">
             <div className="rounded-lg h-64 overflow-hidden">
               <img alt="content" className="object-cover object-center h-full w-full" src="/bg-intro-05.jpg" />
             </div>
             <div className="absolute left-1 top-56 text-center text-xl text-yellow-700 font-semibold rounded-full p-3 bg-gray-100 shadow-lg">02.</div>
             <h2 className="title-font text-lg font-medium text-gray-900 mt-6 mb-3">Easy Table Reservations</h2>
           </div>
-          <div className="relative md:w-1/4 w-10/12 mb-10 shadow-lg">
+          <div className="relative md:w-1/4 w-full mb-10 mx-4 shadow-lg">
             <div className="rounded-lg h-64 overflow-hidden">
               <img alt="content" className="object-cover object-center h-full w-full" src="/blog-08.jpg" />
             </div>
