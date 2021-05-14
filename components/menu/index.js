5d220749fe01531f4d56c7424ddb1962e1d6039b
Abiona Michael <amabiona21@gmail.com>
2021-05-14 09:41:07 +0100

    fixed the css bug in menu

--- components/menu/index.js
+++ components/menu/index.js
@@ -8,27 +8,39 @@ const Menu = () => {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 m-auto w-11/12 md:w-full">
           <div className="relative md:row-span-2">
-            {/* <div className="absolute uppercase py-4 px-6 text-lg rounded-md bg-gray-100 tracking-widest hover:bg-yellow-600 hover:text-gray-100 opacity-90 left-36 top-60 font-semibold">Lunch</div> */}
-            <img src="/our-menu-01.jpg" className="w-full h-full rounded-lg" alt=""/>
+            <img src="/our-menu-01.jpg" className="object-cover w-full h-full rounded-lg" alt=""/>
+            <div className="absolute h-full w-full left-0 top-0 flex flex-col items-center justify-center">
+              <div className="uppercase bg-gray-100 bg-opacity-90 py-2 px-4 md:py-4 md:px-6 md:text-lg font-semibold tracking-widest rounded-md hover:bg-yellow-600 hover:text-gray-100">Lunch</div>
+            </div>
           </div>
           <div className="relative md:row-span-2">
-            {/* <div className="absolute uppercase py-4 px-6 text-lg rounded-md bg-gray-100 tracking-widest hover:bg-yellow-600 hover:text-gray-100 opacity-90 left-36 top-60 font-semibold">Dinner</div> */}
             <img src="/our-menu-05.jpg" className="w-full h-full rounded-lg" alt=""/>
+            <div className="absolute h-full w-full left-0 top-0 flex flex-col items-center justify-center">
+              <div className="uppercase bg-gray-100 bg-opacity-90 py-2 px-4 md:py-4 md:px-6 md:text-lg font-semibold tracking-widest rounded-md hover:bg-yellow-600 hover:text-gray-100">Dinner</div>
+            </div>
           </div>
           <div className="relative">
-            {/* <div className="absolute uppercase py-4 px-6 text-lg rounded-md bg-gray-100 tracking-widest hover:bg-yellow-600 hover:text-gray-100 opacity-90 left-32 top-24  font-semibold">Drinks</div> */}
             <img src="/our-menu-08.jpg" className="w-full h-full rounded-lg" alt=""/>
+            <div className="absolute h-full w-full left-0 top-0 flex flex-col items-center justify-center">
+              <div className="uppercase bg-gray-100 bg-opacity-90 py-2 px-4 md:py-4 md:px-6 md:text-lg font-semibold tracking-widest rounded-md hover:bg-yellow-600 hover:text-gray-100">Drinks</div>
+            </div>
           </div>
           <div className="relative">
-            {/* <div className="absolute uppercase py-4 px-6 text-lg rounded-md bg-gray-100 tracking-widest hover:bg-yellow-600 hover:text-gray-100 opacity-90 left-32 top-24  font-semibold">Starters</div> */}
             <img src="/our-menu-10.jpg" className="w-full h-full rounded-lg" alt=""/>
+            <div className="absolute h-full w-full left-0 top-0 flex flex-col items-center justify-center">
+              <div className="uppercase bg-gray-100 bg-opacity-90 py-2 px-4 md:py-4 md:px-6 md:text-lg font-semibold tracking-widest rounded-md hover:bg-yellow-600 hover:text-gray-100">Starters</div>
+            </div>
           </div>
           <div className="relative md:col-span-2">
-            {/* <div className="absolute uppercase py-4 px-6 text-lg rounded-md bg-gray-100 tracking-widest hover:bg-yellow-600 hover:text-gray-100 opacity-90 left-28 top-8 md:left-80 md:top-24  font-semibold">Happy Hour</div> */}
             <img src="/our-menu-13.jpg" className="w-full h-full rounded-lg" alt=""/>
+            <div className="absolute h-full w-full left-0 top-0 flex flex-col items-center justify-center">
+              <div className="uppercase bg-gray-100 bg-opacity-90 py-2 px-4 md:py-4 md:px-6 md:text-lg font-semibold tracking-widest rounded-md hover:bg-yellow-600 hover:text-gray-100">Happy Hour</div>
+            </div>
           </div>
           <div className="relative">
-            {/* <div className="absolute uppercase py-4 px-6 text-lg rounded-md bg-gray-100 tracking-widest hover:bg-yellow-600 hover:text-gray-100 opacity-90 left-32 top-24  font-semibold">Dessert</div> */}
+            <div className="absolute h-full w-full left-0 top-0 flex flex-col items-center justify-center">
+              <div className="uppercase bg-gray-100 bg-opacity-90 py-2 px-4 md:py-4 md:px-6 md:text-lg font-semibold tracking-widest rounded-md hover:bg-yellow-600 hover:text-gray-100">Dessert</div>
+            </div>
             <img src="/our-menu-16.jpg" className="w-full h-full rounded-lg" alt=""/>
           </div>
         </div>
